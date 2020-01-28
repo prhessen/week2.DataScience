e321c64966d6e5094c91afd73d2eba4c8b9bafa8
Phoebe Hessen <hesse179@umn.edu>
2020-01-28 14:52:34 -0600

    Assignment Completed

--- R/week2.R
+++ R/week2.R
@@ -4,3 +4,9 @@ levels(rt_df$condition) <- c("Control","Experimental")
 levels(rt_df$gender) <- c("Female","Male", "Transgender")
 
 # Analysis
+mean(rt_df$rt)
+rt_f_df <- subset(rt_df, subset = gender == "Female")
+summary(rt_f_df)
+hist(rt_f_df$rt)
+datasets <- list(rt_df, rt_f_df)
+datasets[[1]][2]
